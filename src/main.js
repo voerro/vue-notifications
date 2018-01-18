fd2a0dbc8edae17c1dc4a1369ad0150ec57b75c9
Alexander Zavyalov <whmordred@gmail.com>
2018-01-18 22:27:10 +0800

    Adjustments to make it work as a package

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
-
 import Notifications from './components/Notifications.vue'
 import notify from './interface'
+
 window.notify = notify;
-Vue.component('notifications', Notifications);
 
-new Vue({
-  el: '#app'
-})
+if (document.querySelector('#voerro-notifications-demo-app-nnpqbywihspyskxjjiky')) {
+    Vue.component('notifications', Notifications);
+    
+    new Vue({
+        el: '#voerro-notifications-demo-app-nnpqbywihspyskxjjiky'
+    });
+}
 
-export { Notifications, notify };
+export default Notifications;
